1b4fafee97684b9e2b17cd1ef781630068d9b5f6
Viktoryia802 <villette@list.ru>
2020-06-19 00:12:24 +0200

    lesson 1-2 Heading done

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.sv"
+        alt="smth hotel"
+      />
+      CYF Hotel
+    </header>
+  );
+};
+export default Heading;
