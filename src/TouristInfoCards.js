6ba73c9da5a2438956990034719b1ece716be10b
Viktoryia802 <villette@list.ru>
2020-06-19 01:03:17 +0200

    lesson 1-4 Footer done

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -20,18 +20,21 @@ const TouristInfoCards = () => {
         img="https://live.staticflickr.com/4831/32321524798_f1d2188ae7_b.jpg"
         url="https://www.peoplemakeglasgow.com"
         city="Glasgow"
+        alt="glasqowbuildings"
       />
 
       <Card
         img="https://live.staticflickr.com/6059/6272043372_5cd92b0ea9_b.jpg"
         url="https://www.visitmanchester.com"
         city="Manchester"
+        alt="manchestercityside"
       />
 
       <Card
         img="https://upload.wikimedia.org/wikipedia/commons/8/82/London_Big_Ben_Phone_box.jpg"
         url="https://www.visitlondon.com"
         city="London"
+        alt="bigbenlondon"
       />
     </div>
   );
